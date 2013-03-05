c9fae7973fda42edefb2a23e5f7ab35bc5bf6eb6
Manaal Faruqui <manaalfar@gmail.com>
2013-03-05 02:36:40 -0500

    removed things :P

--- hw2/src/clean.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-for line in sys.stdin:
-    
-    line = line.strip()
-    line = line.replace("&quot;",'"')
-    line = line.replace("&#39;","'")
-    
-    print line.lower()
--- hw2/src/replace.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-for line in sys.stdin:
-	line = line.strip()
-	lines = line.split(' | | | ')
-	for line in lines:
-		print line
